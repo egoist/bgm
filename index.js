822f18520abedd1d12a0360ee955745493ee6ee1
EGOIST <0x142857@gmail.com>
2017-06-27 00:18:26 +0800

    make it work again

--- index.js
+++ index.js
@@ -1,73 +1,49 @@
 'use strict'
 const fetch = require('bgm-fetch')
-const Spin = require('io-spin')
+const ora = require('ora')
 const stringWidth = require('string-width')
 const chalk = require('chalk')
 const indento = require('indento')
+const table = require('text-table')
 
-const spin = new Spin('Box1')
+const spin = ora()
 
 module.exports = function (cli) {
   const season = cli.flags.season
   const weekDay = cli.flags.weekday || new Date().getDay()
-  const proxy = 'http://7d9hn8.com1.z0.glb.clouddn.com/wxt2005/bangumi-list/master/json/bangumi-__SEASON__.json'
+  const proxy = 'http://7d9hn8.com1.z0.glb.clouddn.com/bangumi-data/bangumi-data/master/data/items/{year}/{season}.json'
   spin.start()
-  fetch(season, {format: true, proxy})
-    .then(data => {
+  fetch({
+    format: true,
+    season,
+    proxy
+  }).then(data => {
       spin.stop()
       const today = data[weekDay]
       console.log('\n', indento(chalk.cyan(chineseWeekday(weekDay)), 1), '\n')
-      let t = today
-        .sort((a, b) => {
-          const aTime = getTime(a)
-          const bTime = getTime(b)
-
-          return aTime - bTime
-
-          function getTime(item) {
-            if (item.timeCN) {
-              return parseInt(item.timeCN, 10)
-            } else if (item.timeJP) {
-              return parseInt(item.timeJP, 10) - 100
-            }
-            return 0
-          }
-        })
-        .map(item => {
-          const time = item.timeCN ? `CN: ${formatTime(item.timeCN)}` : `JP: ${formatTime(item.timeJP)}`
-          return [item.titleCN, time]
-        })
-      t = table(t)
-      console.log(indento(t, 2), '\n')
+      console.log(table(today.map(item => [
+        '  '+ item['titleTranslate']['zh-Hans'][0],
+        formatTime(item.begin)
+      ]), {
+        stringLength: stringWidth
+      }))
+      console.log()
     })
     .catch(e => {
       spin.stop()
-      console.log(e.stack)
+      console.error(e.stack)
     })
 }
 
-function table(data) {
-  let max = stringWidth(data[0][0])
-  for (const item of data) {
-    const currentWidth = stringWidth(item[0])
-    if (currentWidth > max) {
-      max = currentWidth
-    }
-  }
-  return data.map(item => {
-    const currentWidth = stringWidth(item[0])
-    if (currentWidth < max) {
-      item[0] += ' '.repeat(parseInt(max - currentWidth, 10))
-    }
-    return item.join('    ')
-  }).join('\n')
-}
-
 function formatTime(time) {
-  time = [time.substr(0, 2), time.substr(2)]
-  return time.join(':')
+  const d = new Date(time)
+  return `${ensureTime(d.getHours())}:${ensureTime(d.getMinutes())}`
 }
 
 function chineseWeekday(day) {
   return ['周日', '周一', '周二', '周三', '周四', '周五', '周六'][day]
+}
+
+function ensureTime(t) {
+  return t < 10 ? `0${t}` : t
 }
